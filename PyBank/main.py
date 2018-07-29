f430c6f88c4df3be95597e17cba3699f54d792f1
David Hickman <dhickman81@hotmail.com>
2018-07-29 12:00:08 -0500

    Add total months and net profit

---
+++ PyBank/main.py
@@ -0,0 +1,35 @@
+# Import the necessary dependencies for os.path.join() import os import csv 
+import os
+import csv
+
+# Initialize variable
+month_count = 0
+net_profit = 0
+ 
+# Read in a .csv file 
+csv_path = os.path.join("..","PyBank", "budget_data.csv") 
+
+with open(csv_path, newline='') as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Check if the csv file has a header row
+    sniffer = csv.Sniffer()
+    contains_hdr = sniffer.has_header(csv_file.read(128))
+
+    # This skips the header row of the CSV file.
+    if contains_hdr == True:
+        next(csv_reader,None)
+
+    # Read each row of data after the header
+    for row in csv_reader:
+        # Count the number of months in the dataset
+        month_count += 1
+        # Net profit
+        net_profit += int(row[1])
+
+    print("Financial Analysis")
+    print("------------------------------")
+    print("Total Months: " + str(month_count))
+    print("Net Profit: " + str(net_profit))
