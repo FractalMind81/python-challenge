3e8675decf2010d966893dc4eaa624c8106c59de
David Hickman <dhickman81@hotmail.com>
2018-07-29 20:42:35 -0500

    add max/min monthly changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,9 @@ import functools
 # Initialize variable
 month_count = 0
 net_profit = 0
-monthly_change = []
 last_month_total = 0
+monthly_change = []
+months = []
  
 # Read in a .csv file 
 csv_path = os.path.join("..","PyBank", "budget_data.csv") 
@@ -37,17 +38,32 @@ with open(csv_path, newline='') as csv_file:
         # Build list of monthly changes
         monthly_change.append(int(row[1]) - last_month_total)
 
-        # Update last month's total
+        # Build list of months
+        months.append(row[0])
+
+        # Update last month total
         last_month_total = int(row[1])
 
-    # Remove the first entry of monthly change
+    # Remove the first entry of monthly change and months
     monthly_change.pop(0)
-    
-    # Avergage change 
-    avg_change = functools.reduce(lambda x,y : x+y,monthly_change)/(month_count-1)
+    months.pop(0)
 
+    # Avergage change 
+    avg_change = sum(item for item in monthly_change)/(month_count-1)
+
+    # Max change/month
+    max_change = max(monthly_change) 
+    max_month = months[monthly_change.index(max_change)]
+
+    # Min change/month
+    min_change = min(monthly_change)
+    min_month = months[monthly_change.index(min_change)]
+
+    # Format output
     print("Financial Analysis")
     print("------------------------------")
     print("Total Months: " + str(month_count))
-    print("Net Profit: " + str(net_profit))
-    print("Average change: " + str(avg_change))
+    print("Net Profit: " + "${:,.2f}".format(net_profit))
+    print("Average change: " + "${:,.2f}".format(avg_change))
+    print("Greatest Increase in Profits: " + max_month + " " + "${:,.2f}".format(max_change))
+    print("Greatest Decrease in Profits: " + min_month + " " + "${:,.2f}".format(min_change))
